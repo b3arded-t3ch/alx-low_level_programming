15eef8393a3ceff97e0dbb243c868fa42277fad1
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-06 01:02:46 +0100

    sizes, modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,10 @@ int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-    
-    // Specify sizes for long int and long long based on the target architecture
-    #ifdef __LP64__
-        printf("Size of a long int: %ld byte(s)\n", sizeof(long));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
-    #else
-        printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-    #endif
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of long loong int: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	
 	return (0);
