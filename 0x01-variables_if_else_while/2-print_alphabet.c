8d8dd1356f3aa8ce4766456023eddc3fbf99763f
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-07 01:59:23 +0100

    print lower

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,17 +5,16 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	char alphabet;
-	alphabet = 'a';
+	char alphabet = 'a';
 
-	while (alphabet<='z')
+	while (alphabet <= 'z')
 	{
 		putchar(alphabet);
 		alphabet++;
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
