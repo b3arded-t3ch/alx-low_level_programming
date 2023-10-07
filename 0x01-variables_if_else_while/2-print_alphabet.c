91d055647c9285015a69c5e4b60365c361ead4b6
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-07 01:20:27 +0100

    print lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char ch = tolower('S');
+	
+	putchar(ch);
+
+	return (0);
+}
