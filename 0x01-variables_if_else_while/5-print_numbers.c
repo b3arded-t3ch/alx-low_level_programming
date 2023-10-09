369715c37b9b1e4b927384c84603b41113a984c9
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-09 15:15:14 +0100

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num >= '0' && num <= '9';)
+		{
+			putchar(num);
+			num++;
+		}
+	putchar('\n');
+	return (0);
+}
