cca83f851eed5040f04098d08788e7e0b4241fe5
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-09 15:45:40 +0100

    Printing numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char num;
 
-	for  (num = '0'; num >= '0' && num <= '9';)
+	for (num = '0'; num >= '0' && num <= '9';)
 	{
 		putchar(num);
 		num++;
 	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }
