332c5e9a6b7d8d0155006d59357e9f5471cd375b
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-10 15:42:00 +0100

    header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
-
+#include <main.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
 int main(void)
 {
-	char str[] = "_putchar\n";
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
