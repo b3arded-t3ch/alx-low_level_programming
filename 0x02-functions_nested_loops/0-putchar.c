dd65ebead0e1cdc267cad2480d5807abe7a5613a
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-10 16:17:59 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,16 @@
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
-int _putchar(char c);
 
 int main(void)
 {
