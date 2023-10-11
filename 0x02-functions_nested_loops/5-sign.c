d6133221d98806ccc2d1052cbc52560ca0a3e207
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-11 14:49:30 +0100

    5 main c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * Return: Always int
+ *
+ */
+int print_sign(int)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if(n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');;
+	}
+}
