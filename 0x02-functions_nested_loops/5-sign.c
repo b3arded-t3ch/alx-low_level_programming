5b1422f8f05853219ed756a48308349c5fd1ff9d
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-11 14:55:21 +0100

    number signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,21 @@
  * Return: Always int
  *
  */
-int print_sign(int)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if(n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
-		_putchar('-');;
 	}
 }
