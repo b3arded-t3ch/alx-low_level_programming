4d919f8d1071f29cb304636d040d0778922c2fdd
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-11 12:42:45 +0100

    4-main.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * test_islower - Test the _islower function
+ *
+ * @c: Number to pass to _islower function
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+	{
+	return 1;
+	}
+	else
+	{
+	return 0;
+	}	
+}	
