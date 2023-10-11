19ee89ee3f0b7c90efd1b8cac064435bdc287dbe
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-11 16:22:04 +0100

    negative numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_abs - computes the absolute value of an integer
+ *
+ *Return: Always in
+ *
+ *@c: paramter used
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c);
+	}
+}
