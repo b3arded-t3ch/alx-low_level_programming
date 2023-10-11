c561da184d6b49b5dbb1a9acab1f5b5885fca509
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-11 10:05:54 +0100

    adding 2-main.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print ten times
+ *
+ * Return: Always void
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+
+	char c;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
