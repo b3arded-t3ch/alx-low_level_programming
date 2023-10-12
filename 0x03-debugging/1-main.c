dbbedb9710fb128b0a47245271ebdbb52121fb18
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-12 12:41:27 +0100

    printf

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-/**	while (i < 10)
-	{
-		putchar(i);
-	}
+/**
+* while - this parts causes infinite loop
+*	while (i < 10)
+*	{
+*		putchar(i);
+*	}
 */
-
 	printf("Infinite loof avoided! \\o/\n");
 
 	return (0);
