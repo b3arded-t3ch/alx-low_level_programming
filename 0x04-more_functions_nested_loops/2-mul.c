327a124d4e1c117f14b3931a979bb7b950d4da82
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-12 21:45:58 +0100

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - It multiplies two digits
+ *
+ * Return: Always an integer
+ *
+ * @a: this is the first parameter
+ * @b: this is the second parameter
+ *
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
