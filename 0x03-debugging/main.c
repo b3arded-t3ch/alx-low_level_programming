51ee3abcb5897f3a52396c976096b67473d5ffdd
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-12 11:26:09 +0100

    Debugging 1

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests wether the function prints positive or not
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	
+	positive_or_negative(i);
+
+	return (0);
+}
