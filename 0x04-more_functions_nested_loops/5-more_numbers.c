fb71e031d26a1a7b81f88fddfd321cad0021575f
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-13 00:04:00 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints digits through 14 ten times
+ *
+ * Return: Always null
+ */
+void more_numbers(void)
+{
+	int i;
+
+	int c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar('1');
+			_putchar(c % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
