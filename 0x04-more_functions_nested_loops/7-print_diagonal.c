47f073112ac5cad99942185b50e618d4aedd845d
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-14 00:54:50 +0100

    Diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,19 @@ void print_diagonal(int n)
 {
 	char c = '\\';
 
-	int i;
+	int i, j;
 
-	for (i = 0; i <= n; i++)
+	if (n < 0 || n == 0)
 	{
-		if ((n == 0) || (n < 0))
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
 		{
 			_putchar(c);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
