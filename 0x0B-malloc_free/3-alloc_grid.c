83f045528da2e92c7fe85baaf8cc908e056b7731
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-10-31 14:47:11 +0100

    allc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,11 +30,13 @@ int **alloc_grid(int width, int height)
 		array[i] = (int *)malloc(width * sizeof(int));
 		if (array[i] == NULL)
 		{
-			return (NULL);
+			for (j = 0; j < i; j++)
+		{
+			free(array[j]);
 		}
-	}
-	for (i = 0; i < height; i++)
-	{
+		free(array);
+		return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
