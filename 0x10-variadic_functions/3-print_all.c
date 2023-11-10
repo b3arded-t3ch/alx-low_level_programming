f4394bbf8602eed0104fddf2df028d9b10e6e470
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2023-11-10 04:21:48 +0100

    print all, modified but incomplete

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,13 +5,12 @@
  * print_all - prints anything
  *
  * Return: Nothing
- * @format: list of types of arguments 
+ * @format: list of types of arguments
  * passed to the function
  */
 void print_all(const char * const format, ...)
 {
 	va_list my_box;
-
 	const char *format_ptr = format;
 
 	char *s;
@@ -24,41 +23,34 @@ void print_all(const char * const format, ...)
 
 	while (*format_ptr)
 	{
-		if (*format_ptr == '%')
+		if (*format_ptr == 'c')
 		{
-			format_ptr++;
-			if (*format_ptr == 'c')
+			c = va_arg(my_box, int);
+			printf("%c", c);
+		}
+		else if (*format_ptr == 'i')
+		{
+			i = va_arg(my_box, int);
+			printf("%d", i);
+		}
+		else if (*format_ptr == 'f')
+		{
+			f = va_arg(my_box, double);
+			printf("%f", f);
+		}
+		else if (*format_ptr == 's')
+		{
+			s = va_arg(my_box, char *);
+			if (s == NULL)
 			{
-				c = va_arg(my_box, int);
-				printf("%c", c);
-			}
-			else if (*format_ptr == 'i')
-			{
-				i = va_arg(my_box, int);
-				printf("%i", i);
-			}
-			else if (*format_ptr == 'f')
-			{
-				f = va_arg(my_box, double);
-				printf("%f", f);
-			}
-			else if (*format_ptr == 's')
-			{
-				s = va_arg(my_box, char *);
-				if (s == NULL)
-				{
-					printf("(nil)");
-				}
-				else
-				{
-					printf("%s", s);
-				}
+				printf("(nil)");
 			}
 			else
 			{
-				format_ptr++;
+				printf("%s", s);
 			}
 		}
+		format_ptr++;
 	}
 	va_end(my_box);
 	printf("\n");
