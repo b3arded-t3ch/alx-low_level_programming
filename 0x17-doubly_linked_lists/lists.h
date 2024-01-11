8a2b7a4c9964a4bcc8fa3be731d735ebfecb389b
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-11 14:52:50 +0100

    header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -14,3 +17,4 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+#endif
