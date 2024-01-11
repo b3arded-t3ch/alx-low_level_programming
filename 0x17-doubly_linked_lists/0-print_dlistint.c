c5d7144ad30ff6812d92b5c30e4e7b0064bd961c
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-11 13:50:46 +0100

    print dl

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/*
+ * print_dlistint - prints elements of a dl list
+ * Return: number of nodes
+ * h: the head node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
