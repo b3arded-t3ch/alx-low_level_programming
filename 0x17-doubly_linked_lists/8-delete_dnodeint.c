088623fe82c33dd6d313edea2db95f68649c0ff9
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-12 08:20:36 +0100

    incomplete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (temp == *head)
 			{
 				*head = (*head)->next;
-				if (*head == NULL)
+				if (*head != NULL)
 				{
 					(*head)->prev = NULL;
 				}
