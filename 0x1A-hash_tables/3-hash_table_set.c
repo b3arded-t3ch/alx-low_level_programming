8224f30260395af26f1f0bd55b415c9260e7c759
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-02-03 00:25:43 +0100

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,40 +12,51 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int key_dex;
-	hash_node_t *new_node;
 	hash_node_t *current_node;
+	hash_node_t *new_node;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	{
+		return (0);
+	}
 
 	key_dex = hash_djb2((const unsigned char *)key) % ht->size;
 
+	current_node = ht->array[key_dex];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		current_node = current_node->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		return (0);
 	}
-
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key == NULL || new_node->value == NULL)
+	if (new_node->key == NULL)
 	{
-		free(new_node->key);
-		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-	new_node->next = NULL;
-	if (ht->array[key_dex] == NULL)
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
 	{
-		ht->array[key_dex] = new_node;
+		free(new_node->key);
+		free(new_node);
+		return (0);
 	}
-	else
-	{
-		current_node = ht->array[key_dex];
-		while (current_node->next != NULL)
-		{
-			current_node = current_node->next;
-		}
-		current_node->next = new_node;
-	}
+	new_node->next = ht->array[key_dex];
+	ht->array[key_dex] = new_node;
+
 	return (1);
 }
